8018b20999cbb6f3f17d4f257ec8c7f149e16d9e
Olivia Guerrero <112516748+OliveBattle@users.noreply.github.com>
2023-02-15 03:34:12 +0000

    collatz and mastermind

--- src/main/scala/mastermind/Mastermind.scala
+++ src/main/scala/mastermind/Mastermind.scala
@@ -13,19 +13,28 @@ val validColors = List('B', 'Y', 'R', 'G')
 
 /** Get a random color from the list of valid colors */
 def getRandomColor(): Color =
-  ???
+  validColors(Random.nextInt(4))
 
 /** Given four colors, make a board from them */
 def makeBoardFromColors(c1: Color, c2: Color, c3: Color, c4: Color): Board =
-  ???
+  // List(c1, c2, c3, c4).mkString()  // mkString refused to work
+  c1.toString() + c2 + c3 + c4
 
 /** Create a random board */
 def getRandomBoard(): Board =
-  ???
+  makeBoardFromColors(getRandomColor(), getRandomColor(),
+                      getRandomColor(), getRandomColor())
 
 /** Play one round of the game */
 def playRound(board: Board): (Int, Int) =
-  ???
+  val c1 = readLine("Enter a guess for spot 1: ")(0) 
+      //the (0) grabs only the first char
+
+  val c2 = readLine("Enter a guess for spot 2: ")(0)
+  val c3 = readLine("Enter a guess for spot 3: ")(0)
+  val c4 = readLine("Enter a guess for spot 4: ")(0)
+  val guessBoard = makeBoardFromColors(c1, c2, c3, c4)
+  scoreGuess(board, guessBoard)
 
 /** Score a guess
   *
